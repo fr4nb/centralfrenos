615d0fd6a524575b696fccdd297a71929996f8c9
Francisco <franb.mail@gmail.com>
2018-12-01 14:05:56 -0300

    trabajando

--- js/rutas.js
+++ js/rutas.js
@@ -3,7 +3,7 @@
 	var _frame = _.getID('vista');
 
 	_frame.enrutar()
-		.ruta('/', 'vistas/home.html', null, null)
+		.ruta('/', 'vistas/home.html', 'js/home.js', null)
 		.ruta('/productos','vistas/prod.html', null, null);
 
 })(window, document); 
