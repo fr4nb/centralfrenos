a303723a69caa67f30e03b1cefce9b63cc576c46
Francisco <franb.mail@gmail.com>
2018-12-02 15:06:51 -0300

    controladores inicio

---
+++ js/controladores/productos.js
@@ -0,0 +1,11 @@
+(function (window, document) {
+	
+	_.controlador('productos', {
+
+		$('#prodModal').on('shown.bs.modal', function(){
+      		console.log('modalContent');
+    	});
+
+	});
+
+})(window, document); 
--- js/rutas.js
+++ js/rutas.js
@@ -3,7 +3,9 @@
 	var _frame = _.getID('vista');
 
 	_frame.enrutar()
-		.ruta('/', 'vistas/home.html', 'js/home.js', null)
-		.ruta('/productos','vistas/prod.html', null, null);
+		.ruta('/', 'vistas/home.html', null, null)
+		.ruta('/productos','vistas/prod.html', 'productos', null)
+		.ruta('/servicios-CentralFrenos','vistas/servicios-central_frenos.html',null,null)
+		.ruta('/servicios-TecnoFrenos','vistas/servicios-tecno_frenos.html',null,null);
 
 })(window, document); 
